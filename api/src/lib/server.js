d09708a53d3664521cf6aa028bf9d355330277c7
ZkiStr <zaki.satriapr@gmail.com>
2025-01-29 17:33:18 +0700

    Add Docker support and environment configuration for development

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import config from "./config.js";
 
 export const startServer = () => {
   const httpServer = express();
+  const port = config.port;
 
   try {
-    httpServer.listen(3000, () => {
-      console.log(`Server running on port 3000`);
+
+    console.log("Testing docker images");
+    
+    httpServer.listen(port, () => {
+      console.log(`Server running on port ${port}`);
     });
   } catch (err) {
     throw new Error(err);
